1213153a7df31df22d61ab80a2010bee783ee6a8
Simon Ziervogel <simon-ziervogel@web.de>
2024-09-21 15:09:50 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 @Component({
 
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 
 })
 export class AppComponent {
